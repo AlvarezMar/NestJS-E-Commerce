2de4a713a8c342de96dbb953a1c2e6e3fcc57104
AlvarezMar <alvarez.majuan@hotmail.com>
2024-05-10 15:11:27 -0600

    Render DB Host Config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { loggerGlobal } from './middlewares/logger.middleware';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -25,6 +27,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
